4cdec1bb02aa0783e6dd3d610d064775cc98f456
Panayiotis Gavriil <pan.gav2001@gmail.com>
2023-02-17 11:35:17 -0500

    Added high-level API to add rules to Firewall

--- Firewall/eBPF/load_xdp.c
+++ Firewall/eBPF/load_xdp.c
@@ -9,79 +9,195 @@
 #define IFNAME "xdptut-080a"
 
 #define FORBIDDEN_DST_IP 16845578 // 10.11.1.1
+#define FORBIDDEN_SRC_IP 33622794 // 10.11.1.2
+#define IP_ICMP 1
+#define FORBIDDEN_PROTO IP_ICMP
+#define PATH "./eBPFirewall_kernel.o"
 
 struct boolean
 {
     unsigned int present : 1;
 };
 
+struct bpf_object *bpf_object_open(char *path)
+{
+    return bpf_object__open(path);
+}
+
+const char *bpf_object_name(const struct bpf_object *obj)
+{
+    return bpf_object__name(obj);
+}
+
+void bpf_object_print_name(const struct bpf_object *obj)
+{
+    const char *name = bpf_object_name(obj);
+    if (name)
+    {
+        printf("The object name is: %s\n", name);
+    }
+    else
+    {
+        printf("There was an error getting the name for this BPF object!\n");
+        // should print error
+    }
+}
+
+void bpf_object_load(struct bpf_object *obj)
+{
+    int loaded = bpf_object__load(obj);
+    if (!loaded)
+    {
+        printf("The BPF object was loaded successfully!\n");
+    }
+    else
+    {
+        printf("There was an error loading the BPF object!\n");
+        // should print error
+    }
+    bpf_object_print_name(obj);
+}
+
+struct bpf_program *bpf_object_find_program_by_name(const struct bpf_object *obj, const char *name)
+{
+    struct bpf_program *bprog = bpf_object__find_program_by_name(obj, name);
+    if (!bprog)
+    {
+        printf("No BPF program with name %s is found in BPF object provided!\n", name);
+        bpf_object_print_name(obj);
+        // should print error
+    }
+    return bprog;
+}
+
+int bpf_program_fd(const struct bpf_program *prog)
+{
+    int fd = bpf_program__fd(prog);
+    if (!fd)
+    {
+        printf("There was an error getting the file descriptor for the BPF provided!\n");
+        // should print error
+    }
+    return fd;
+}
+
+void bpf_program_print_fd(const struct bpf_program *prog)
+{
+    int fd = bpf_program_fd(prog);
+    if (fd)
+    {
+        printf("The BPF program file descriptor is %d.\n", fd);
+    }
+}
+
+void bpf_xdp_attach_SKB_simple(int ifindex, int prog_fd)
+{
+    int attached = bpf_xdp_attach(ifindex, prog_fd, XDP_FLAGS_SKB_MODE, 0);
+    if (attached)
+    {
+        printf("There was an error attaching the BPF program with file descriptor %d to interface %d!\n", prog_fd, ifindex);
+        // should print error
+    }
+}
+
+struct bpf_map *bpf_object_find_map_by_name(const struct bpf_object *obj, const char *name)
+{
+    struct bpf_map *map = bpf_object__find_map_by_name(obj, name);
+    if (!map)
+    {
+        printf("No map named %s could be found in the given BPF object!\n", name);
+        bpf_object_print_name(obj);
+        // should print error
+    }
+    return map;
+}
+
+int bpf_object_find_map_fd_by_name(const struct bpf_object *obj, const char *name)
+{
+    int map_fd = bpf_object__find_map_fd_by_name(obj, name);
+    if (map_fd < 0)
+    {
+        printf("There wqs an error getting the file descriptor for the map named %s associated with the given BPF object!\n", name);
+        bpf_object_print_name(obj);
+        // should print error
+    }
+    return map_fd;
+}
+
+void bpf_map_update_elem_simple(const struct bpf_object *obj, const char *map_name, const void *key, size_t key_sz, const void *value, size_t value_sz)
+{
+    struct bpf_map *map = bpf_object_find_map_by_name(obj, map_name);
+    int updated = bpf_map__update_elem(map, key, key_sz, value, value_sz, BPF_ANY);
+    if (updated)
+    {
+        printf("There was an error updating element with given key in map named %s!\n", map_name);
+        // should print error
+    }
+}
+
+void rule_update(const struct bpf_object *obj, const char *rule, const void *key, size_t key_size, const void *add)
+{
+    struct boolean value = {.present = *(const bool *)add};
+    // probably (surely) not very (at all) safe for production
+    char map_name[20] = "forbidden_";
+    bpf_map_update_elem_simple(obj, strcat(map_name, rule), key, key_size, &value, sizeof(value));
+}
+
+void forbidden_dst_ip(const struct bpf_object *obj, const void *dst_ip, const bool add)
+{
+    rule_update(obj, "dst_ips", dst_ip, sizeof(__be32), &add);
+}
+
+void forbidden_src_ip(const struct bpf_object *obj, const void *src_ip, const bool add)
+{
+    rule_update(obj, "src_ips", src_ip, sizeof(__be32), &add);
+}
+
+void forbidden_dst_ports(const struct bpf_object *obj, const void *dst_port, const bool add)
+{
+    rule_update(obj, "protocols", dst_port, sizeof(__be16), &add);
+}
+
+void forbidden_protocol(const struct bpf_object *obj, const void *proto, const bool add)
+{
+    rule_update(obj, "protocols", proto, sizeof(__u8), &add);
+}
+
 int main(int argc, char **argv)
 {
-    int ifindex, loaded, btf_if, bprog_fd, attached, map_fd, updated, contained;
-
-    char *path;
-    const char *bobj_name, *bprog_name;
+    int ifindex, bprog_fd;
 
     struct bpf_object *bobj;
     struct bpf_program *bprog;
-    struct bpf_map *map;
 
-    __be32 key;
-    struct boolean value;
+    __be32 key_32;
+    __u8 key_8;
 
     ifindex = if_nametoindex(IFNAME);
 
     printf("The interface index of %s is:  %d\n", IFNAME, ifindex);
 
-    path = "/home/kali/Desktop/git/github/own/eBPFunctions/Firewall/eBPF/eBPFirewall_kernel.o";
+    bobj = bpf_object_open(PATH);
 
-    bobj = bpf_object__open(path);
+    bpf_object_load(bobj);
 
-    bobj_name = bpf_object__name(bobj);
+    bprog = bpf_object_find_program_by_name(bobj, "xdp_firewall_prog");
 
-    printf("The object name is: %s\n", bobj_name);
+    bprog_fd = bpf_program_fd(bprog);
 
-    loaded = bpf_object__load(bobj);
+    bpf_xdp_attach_SKB_simple(ifindex, bprog_fd);
 
-    printf("If zero then program was loaded: %d\n", loaded);
+    key_32 = FORBIDDEN_DST_IP;
 
-    btf_if = bpf_object__btf_fd(bobj);
+    forbidden_dst_ip(bobj, &key_32, false);
 
-    printf("BTF fd is: %d\n", btf_if);
+    key_32 = FORBIDDEN_SRC_IP;
 
-    bprog = bpf_object__find_program_by_name(bobj, "xdp_firewall_prog");
+    forbidden_src_ip(bobj, &key_32, false);
 
-    bprog_name = bpf_program__section_name(bprog);
+    key_8 = FORBIDDEN_PROTO;
 
-    printf("The program section name is %s\n", bprog_name);
-
-    bprog_fd = bpf_program__fd(bprog);
-
-    printf("The prog fd is: %d\n", bprog_fd);
-
-    attached = bpf_xdp_attach(ifindex, bprog_fd, XDP_FLAGS_SKB_MODE, 0);
-
-    if (attached)
-        printf("%d\n", attached);
-
-    map = bpf_object__find_map_by_name(bobj, "forbidden_dst_ips");
-
-    if (!map)
-        printf("No such map!\n");
-
-    map_fd = bpf_object__find_map_fd_by_name(bobj, "forbidden_dst_ips");
-
-    key = FORBIDDEN_DST_IP;
-
-    value.present = 1;
-
-    updated = bpf_map__update_elem(map, &key, sizeof(__be32), &value, sizeof(struct boolean), BPF_ANY);
-
-    printf("%d\n", updated);
-
-    contained = bpf_map_lookup_elem(map_fd, &key, &value);
-
-    printf("%d\n", value.present);
+    forbidden_protocol(bobj, &key_8, false);
 
     // bpf_xdp_detach(ifindex, XDP_FLAGS_SKB_MODE, 0);
 
